5e1f1c47e263fbd5add80763b201e728538120fb
Beknazar007 <saitovbeknazar6@gmail.com>
2023-08-30 04:58:38 +0600

    adding jenkin pipe

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  name = "instance-one"
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyExampleInstance"
+  }
 }
