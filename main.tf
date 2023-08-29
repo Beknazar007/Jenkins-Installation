7155f6b299236eb01432796ec29d9f8b13300ac6
Beknazar007 <saitovbeknazar6@gmail.com>
2023-08-30 04:35:48 +0600

    adding jenkin pipe

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
-terraform {
-  backend "s3" {
-    bucket = "for-project-in-aip-terraform-deploy"
-    key    = "remote.tfstate"
-    region = "us-east-1"
-  }
-}
+
 resource "aws_instance" "example" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
